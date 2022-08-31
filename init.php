06b1d0fea5e72683fe0e5e6c4178d8c16aff0789
Utya <truejuve@gmail.com>
2022-08-31 17:35:19 +0300

    add initial-structure

---
+++ init.php
@@ -0,0 +1,12 @@
+<?php
+
+const JOIN_CORE=true;
+
+spl_autoload_register(function ($class) {
+    $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+session_start();
